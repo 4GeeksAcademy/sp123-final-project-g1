0fe3b86f166f8c4202be60aba0bc741c41a4b755
Dylan Steven Inchaurregui Dooley <dylan.zephlyn@hotmail.com>
2026-01-26 18:01:38 +0000

    [front] wip: esceleto echo para poder terminar JWT

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,16 +3,22 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<nav className="navbar navbar-expand-lg fixed-top" style={{ background: "linear-gradient(to bottom, rgba(128,0,255,0.85), rgba(128,0,255,0))", backdropFilter: "blur(6px)" }}>
+			<div className="container-fluid px-4">
+				<Link className="navbar-brand text-white fw-semibold" to="/" style={{ letterSpacing: "0.15em" }}> SONORA </Link>
+					<div className="collapse navbar-collapse" id="sonoraNav">
+						<ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-lg-4">
+							<li className="nav-item">
+								<Link className="nav-link text-white" to="/explore">Explore</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link text-white" to="/artists">Artists</Link>
+							</li>
+						</ul>
+						<Link to="/loginsignup" className="btn btn-outline-light">
+							Login / Signup
+						</Link>
+					</div>
 			</div>
 		</nav>
 	);
